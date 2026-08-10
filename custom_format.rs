11a79923a015f971075830e94d011cd979f4e002
Vaibhav Brid <vaibhavbrid@users.noreply.github.com>
2026-08-10 15:11:15 -0700

    Implement custom formatting for struct

---
+++ custom_format.rs
@@ -0,0 +1,17 @@
+use std::fmt;
+
+struct Point {
+  x: i32,
+  y: i32,
+}
+
+impl fmt::Display for Point {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "({}, {})", self.x, self.y)
+  }
+}
+
+fn main() {
+  let p = Point {x: 5, y: 10 };
+  println!("{}", p);
+}
